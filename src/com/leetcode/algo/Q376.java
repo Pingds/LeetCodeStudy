47a78daff35cef5a35fb2142de0fada7cfa85139
davisping <davisping@163.com>
2020-12-13 11:22:55 +0800

    [Dev]fix [0,0] Q376;

--- src/com/leetcode/algo/Q376.java
+++ src/com/leetcode/algo/Q376.java
@@ -8,7 +8,7 @@ package com.leetcode.algo;
  */
 public class Q376 {
     public static void main(String[] args){
-        int[] numbers =new int[]{1,17,5,10,13,15,10,5,16,8};
+        int[] numbers =new int[]{0,0};
         int result = wiggleMaxLength(numbers);
         System.out.println("result:"+ result);
     }
@@ -18,7 +18,7 @@ public class Q376 {
         for(int i=1; i<nums.length; i++){
             if(nums[i]>nums[i-1])
                 up=down+1;
-            else
+            else if(nums[i]<nums[i-1])
                 down=up+1;
         }
 
